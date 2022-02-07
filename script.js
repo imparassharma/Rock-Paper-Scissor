dda65b1678f13e8c0234e3bcf1d0a257701379bd
Paras Sharma <parasangiras121@gmail.com>
2022-02-07 21:20:39 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissor = document.querySelector(".scissors");
-
 const mid = document.querySelector(".mid-section");
 const play = document.querySelector(".mid-section.play");
 
@@ -11,139 +7,102 @@ const result = document.getElementById("result");
 const score = document.getElementById("score");
 let points =0;
 
+const rps = document.querySelectorAll(".rps");
+
 const rule_button = document.querySelector(".button");
 
-rock.addEventListener("click",function(e){
-    mid.classList.add("hidden");
-    play.classList.remove("hidden");
-
-    const yourpick = e.currentTarget.className;
-    var player = row1.children[0];
-    player.className = yourpick;
-    player.innerHTML = "<img src=\"images/icon-"+yourpick+".svg\" alt=\"\">";
-
-    var computer = row1.children[1];
-    const random= ["paper","rock","scissors"] ;
-    const random_no =  Math.floor(Math.random()*3);
-    console.log(random_no);
-    const computerpick = random[random_no];
-    computer.className = computerpick;
-    computer.innerHTML = "<img src=\"images/icon-"+computerpick+".svg\" alt=\"\">"
-
-    if(yourpick=="rock" && computerpick =="paper")
-    {
-        result.innerHTML="YOU LOST!";
-        points--;
-        if(points<0)
-        {
-            points =0;
-        }
-        score.innerHTML=points;
-    }
-    else if(yourpick=="rock" && computerpick =="rock")
-    {
-        result.innerHTML="DRAW!";
-    }
-    else{
-        result.innerHTML="YOU WON!";
-        points++;
-        score.innerHTML=points;
-    }
-
-    rule_button.innerHTML="B A C K";
-    rule_button.addEventListener("click",function(){
-        play.classList.add("hidden");
-        mid.classList.remove("hidden");
-        rule_button.innerHTML="<a href=\"rules.html\">RULES</a>";
-    })  
-})
-paper.addEventListener("click",function(e){
-    mid.classList.add("hidden");
-    play.classList.remove("hidden");
-
-    const yourpick = e.currentTarget.className;
-    var player = row1.children[0];
-    player.className = yourpick;
-    player.innerHTML = "<img src=\"images/icon-"+yourpick+".svg\" alt=\"\">";
-
-    var computer = row1.children[1];
-    const random= ["paper","rock","scissors"] ;
-    const random_no =  Math.floor(Math.random()*3);
-    console.log(random_no);
-    const computerpick = random[random_no];
-    computer.className = computerpick;
-    computer.innerHTML = "<img src=\"images/icon-"+computerpick+".svg\" alt=\"\">"
-
+rps.forEach(function(r){
+    r.addEventListener("click",function(e){
+        mid.classList.add("hidden");
+        play.classList.remove("hidden");
     
-    if(yourpick=="paper" && computerpick =="scissors")
-    {
-        result.innerHTML="YOU LOST!";
-        points--;
-        if(points<0)
-        {
-            points =0;
-        }
-        score.innerHTML=points;     
-    }
-    else if(yourpick=="paper" && computerpick =="paper")
-    {
-        result.innerHTML="DRAW!";
-    }
-    else{
-        result.innerHTML="YOU WON!";
-        points++;
-        score.innerHTML=points;
-    }
-    rule_button.innerHTML="B A C K";
-    rule_button.addEventListener("click",function(){
-        play.classList.add("hidden");
-        mid.classList.remove("hidden");
-        rule_button.innerHTML="<a href=\"rules.html\">RULES</a>";
-    })  
-})
-
-scissor.addEventListener("click",function(e){
-    mid.classList.add("hidden");
-    play.classList.remove("hidden");
-
-    const yourpick = e.currentTarget.className;
-    var player = row1.children[0];
-    player.className = yourpick;
-    player.innerHTML = "<img src=\"images/icon-"+yourpick+".svg\" alt=\"\">";
-
-    var computer = row1.children[1];
-    const random= ["paper","rock","scissors"] ;
-    const random_no =  Math.floor(Math.random()*3);
-    console.log(random_no);
-    const computerpick = random[random_no];
-    computer.className = computerpick;
-    computer.innerHTML = "<img src=\"images/icon-"+computerpick+".svg\" alt=\"\">";
+        const yourpick = e.currentTarget.className;
+        var player = row1.children[0];
+        console.log(row1);
+        player.className = yourpick;
+        player.innerHTML = "<img src=\"images/icon-"+yourpick+".svg\" alt=\"\">";
     
-    if(yourpick=="scissors" && computerpick =="rock")
-    {
-        result.innerHTML="YOU LOST!";
-        points--;
-        if(points<0)
+        var computer = row1.children[1];
+        const random= ["paper","rock","scissors"] ;
+        const random_no =  Math.floor(Math.random()*3);
+        console.log(random_no);
+        const computerpick = random[random_no];
+        computer.className = computerpick;
+        computer.innerHTML = "<img src=\"images/icon-rps "+computerpick+".svg\" alt=\"\">"
+       
+        if(yourpick=="rps rock" && computerpick =="paper")
         {
-            points =0;
+            result.innerHTML="YOU LOST!";
+            points--;
+            if(points<0)
+            {
+                points =0;
+            }
+            score.innerHTML=points;
         }
-        score.innerHTML=points;
-    }
-    else if(yourpick=="scissors" && computerpick =="scissors")
-    {
-        result.innerHTML="DRAW!";
-    }
-    else{
-        result.innerHTML="YOU WON!";
-        points++;
-        score.innerHTML=points;
-    }
-    rule_button.innerHTML="B A C K";
-    rule_button.addEventListener("click",function(){
-        play.classList.add("hidden");
-        mid.classList.remove("hidden");
-        rule_button.innerHTML="<a href=\"rules.html\">RULES</a>";
-    })  
+
+        else if(yourpick=="rps rock" && computerpick =="rock")
+        {
+            result.innerHTML="DRAW!";
+        }
+
+        else if(yourpick=="rps rock" && computerpick =="scissors"){
+            result.innerHTML="YOU WON!";
+            points++;
+            score.innerHTML=points;
+        }
+
+        else if(yourpick=="rps scissors" && computerpick =="rock")
+        {
+            result.innerHTML="YOU LOST!";
+            points--;
+            if(points<0)
+            {
+                points =0;
+            }
+            score.innerHTML=points;
+        }
+
+        else if(yourpick=="rps scissors" && computerpick =="scissors")
+        {
+            result.innerHTML="DRAW!";
+        }
+
+        else if(yourpick=="rps scissors" && computerpick =="paper"){
+            result.innerHTML="YOU WON!";
+            points++;
+            score.innerHTML=points;
+        }
+
+        else if(yourpick=="rps paper" && computerpick =="scissors")
+        {
+            result.innerHTML="YOU LOST!";
+            points--;
+            if(points<0)
+            {
+                points =0;
+            }
+            score.innerHTML=points;
+        }
+
+        else if(yourpick=="rps paper" && computerpick =="paper")
+        {
+            result.innerHTML="DRAW!";
+        }
+
+        else if(yourpick=="rps paper" && computerpick =="rock"){
+            result.innerHTML="YOU WON!";
+            points++;
+            score.innerHTML=points;
+        }
+        
+    
+        rule_button.innerHTML="B A C K";
+        rule_button.addEventListener("click",function(){
+            play.classList.add("hidden");
+            mid.classList.remove("hidden");
+            rule_button.innerHTML="<a href=\"rules.html\">RULES</a>";
+        })  
+    })
+    
 })
-
-
